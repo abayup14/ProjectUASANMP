edc9f7005e38c39a5ecb76908d7330aea29b265c
Andreas Bayu Prakasa <119108290+abayup14@users.noreply.github.com>
2024-06-24 16:31:35 +0700

    Update

--- app/src/main/java/com/example/hobbyapp/model/Model.kt
+++ app/src/main/java/com/example/hobbyapp/model/Model.kt
@@ -1,2 +1,9 @@
 package com.example.hobbyapp.model
 
+data class News (
+    var id: Int,
+    var judul: String,
+    var deskripsi_singkat : String,
+    var category: String,
+    var pengarang: Int
+)
--- app/src/main/java/com/example/hobbyapp/view/MainActivity.kt
+++ app/src/main/java/com/example/hobbyapp/view/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.hobbyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.hobbyapp.R
+import com.example.hobbyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        navController = (supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment).navController
+//        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+//        return super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
